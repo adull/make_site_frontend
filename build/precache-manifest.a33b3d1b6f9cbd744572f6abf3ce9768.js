1f4631df28095ebc4f6ba3598082f58337ffa12b
adull <adlaiabdelrazaq@gmail.com>
2019-03-24 17:07:08 -0500

    added build dir to git

---
+++ build/precache-manifest.a33b3d1b6f9cbd744572f6abf3ce9768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "34545aea6c0f0575be0f2019f60acc7c",
+    "url": "/static/media/snfs.34545aea.otf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "839cdaa906db26e80ef6",
+    "url": "/static/js/main.839cdaa9.chunk.js"
+  },
+  {
+    "revision": "95319d1e787b6fdb8302",
+    "url": "/static/js/1.95319d1e.chunk.js"
+  },
+  {
+    "revision": "839cdaa906db26e80ef6",
+    "url": "/static/css/main.3d84ee0f.chunk.css"
+  },
+  {
+    "revision": "95319d1e787b6fdb8302",
+    "url": "/static/css/1.4b81c60a.chunk.css"
+  },
+  {
+    "revision": "ef8530a5116459980d6335a21a2ac3a5",
+    "url": "/index.html"
+  }
+];
