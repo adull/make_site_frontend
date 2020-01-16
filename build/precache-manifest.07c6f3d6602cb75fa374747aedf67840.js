1cc9cd9d5f8a78123c8d5045098b3929fbf95798
adull <adlaiabdelrazaq@gmail.com>
2020-01-16 15:15:43 -0600

    Built changes to comments

---
+++ build/precache-manifest.07c6f3d6602cb75fa374747aedf67840.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601f1310e68cba37b59a620df60b6fcb",
+    "url": "/sites/index.html"
+  },
+  {
+    "revision": "f89bf2f079df17d034d8",
+    "url": "/sites/static/css/2.2fe65d49.chunk.css"
+  },
+  {
+    "revision": "dee07c49bc16defaf51a",
+    "url": "/sites/static/css/main.e54c3744.chunk.css"
+  },
+  {
+    "revision": "f89bf2f079df17d034d8",
+    "url": "/sites/static/js/2.7c9a3bea.chunk.js"
+  },
+  {
+    "revision": "97d4b1e19106888f34dab18d3a6891bf",
+    "url": "/sites/static/js/2.7c9a3bea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dee07c49bc16defaf51a",
+    "url": "/sites/static/js/main.51fcfffb.chunk.js"
+  },
+  {
+    "revision": "be72c9382d3224bc88e8",
+    "url": "/sites/static/js/runtime-main.99cc492f.js"
+  },
+  {
+    "revision": "34545aea6c0f0575be0f2019f60acc7c",
+    "url": "/sites/static/media/snfs.34545aea.otf"
+  }
+]);
